f6df3e615f942b78ac8bdc462d1d787c2943b568
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-12 11:06:27 +0530

    updated the page with responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -11,18 +11,18 @@ export default function Home() {
       <Image className='w-full  h-auto' src={hero} alt="" />
       <p className='absolute md:top-52 top-[109px] text-[12px] right-14 text-center md:right-48 md:font-bold md:text-5xl'> REDD+ Models <br/> Aligned with <br/> Global <br/> Standards</p>
       </div>     
-      <div className='flex justify-between my-8'>
+      <div className='flex md:flex-row flex-col-reverse justify-between my-4 md:my-8'>
         <div className='text-[#FFF]'>
-          <p className='text-6xl font-normal mx-20'>REDD+ Models <br/> Aligned with Global Standards</p>
-          <p className='text-3xl font-light mt-10 mx-20 mr-32'>Adopt<span className=' font-bold'> Nature-based <br/>
+          <p className='md:text-6xl text-3xl font-normal mt-5 md:mt-0 mx-10 md:mx-20'>REDD+ Models <br/> Aligned with Global Standards</p>
+          <p className='md:text-3xl text-lg font-light mt-5 md:mt-10 mx-10 md:mx-20 mr-32'>Adopt<span className=' font-bold'> Nature-based <br/>
 Solutions </span>with Speed, Scale 
 & Integrity</p>
-<p className='mx-20 mt-10'>Our web-based platform, Chloris, pioneers a new era in forest carbon intelligence. We harness the power of space to provide direct estimates of carbon stock and change. From massive deforestation to the subtle nuances of growth and regrowth, Chloris interprets the language of our planet. No site is too vast or too small — Chloris paints a comprehensive picture for effective climate and nature action. Our customers leverage Chloris insights and analytics to navigate the complex landscape of conservation</p>
-<div className='mx-20 flex justify-center mt-10'>
+<p className='mx-10 md:mx-20 mt-5 md:mt-10'>Our web-based platform, Chloris, pioneers a new era in forest carbon intelligence. We harness the power of space to provide direct estimates of carbon stock and change. From massive deforestation to the subtle nuances of growth and regrowth, Chloris interprets the language of our planet. No site is too vast or too small — Chloris paints a comprehensive picture for effective climate and nature action. Our customers leverage Chloris insights and analytics to navigate the complex landscape of conservation</p>
+<div className='mx-10 md:mx-20 flex justify-center mt-5 md:mt-10'>
         <button className='border border-white px-6 py-3'> CONTACT US</button>
         </div>
         </div>
-        <Image src={model} alt="" className='mt-28' />
+        <Image src={model} alt="" className='md:mt-28 mt-5' />
       </div>
     </main>
   )
