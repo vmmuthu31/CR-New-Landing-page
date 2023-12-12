179b615b35fd7d177bbfecaac89dc69a269d954c
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-12 10:28:18 +0530

    upated the navbar with the logo

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from "../../assets/logo.png"
+import Image from 'next/image'
 
 function Navbar() {
   return (
     <div className="bg-white">
-        
+        <div className='flex'>
+            <Image src={logo} alt="" />
+        </div>
     </div>
   )
 }
