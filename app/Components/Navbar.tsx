66df0eb8fe392402697d1848f68752e2de01cb01
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-12 10:52:29 +0530

    updated the hero section

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -5,8 +5,11 @@ import Image from 'next/image'
 function Navbar() {
   return (
     <div className="bg-white">
-        <div className='flex'>
+        <div className='flex items-center justify-between py-1 px-10'>
             <Image src={logo} alt="" />
+            <div>
+            <button className='text-[#000821] border-2 px-7 py-2  border-[#000821]'>LOGIN</button>
+            </div>
         </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import Navbar from './Components/Navbar'
+import hero from "../assets/hero.png";
 
 export default function Home() {
   return (
     <main className=" bg-[#000821]  min-h-screen  pb-10">
-     
+      <Navbar />
+      <div>
+      <Image className='w-full  h-auto' src={hero} alt="" />
+      <p className='absolute md:top-52 top-[109px] text-[12px] right-14 text-center md:right-48 md:font-bold md:text-5xl'> REDD+ Models <br/> Aligned with <br/> Global <br/> Standards</p>
+      </div>     
     </main>
   )
 }
