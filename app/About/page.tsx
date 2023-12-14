ffd99a1002ea1f92f789c0f011fbb1f79bcb2d16
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-14 19:33:51 +0530

    updated the navbar with the about section

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -7,17 +7,17 @@ function page() {
   return (
     <div>
       <Navbar />
-      <div>
-        <div>
-          <p>About Us</p>
-          <p>Research and Development for Conservation in the Indian Subcontinent.</p>
-          <p>As a dedicated research and development company, our focus lies in the realm of conservation across the Indian subcontinent. Our core services encompass data analysis, advanced modelling, and in-depth policy research. Our objective is to optimise carbon removal and avoidance strategies through the implementation of nature-based solutions.</p>
-          <p>Our proficiency in handling intricate datasets and our prowess in predictive modelling play a crucial role in facilitating effective planning and implementation of conservation strategies. Additionally, our comprehensive policy research is designed to inform and assist policymakers at the forefront of the carbon market.</p>
+      <div className='flex md:justify-between md:flex-row flex-col-reverse md:space-x-10 md:py-20 px-10 py-5 md:px-20'>
+        <div className=' md:w-[890px]'>
+          <p className='md:text-4xl  font-bold'>About Us</p>
+          <p className='my-5'>Research and Development for Conservation in the Indian Subcontinent.</p>
+          <p className='my-5'>As a dedicated research and development company, our focus lies in the realm of conservation across the Indian subcontinent. Our core services encompass data analysis, advanced modelling, and in-depth policy research. Our objective is to optimise carbon removal and avoidance strategies through the implementation of nature-based solutions.</p>
+          <p className='my-5'>Our proficiency in handling intricate datasets and our prowess in predictive modelling play a crucial role in facilitating effective planning and implementation of conservation strategies. Additionally, our comprehensive policy research is designed to inform and assist policymakers at the forefront of the carbon market.</p>
           <p>We are committed to conducting thorough research and due diligence on Voluntary Carbon Market (VCM) projects. This forms the foundation of our support to partners, aiding them in making informed decisions backed by robust research.</p>
         </div>
-        <Image src={logo} alt="" />
+        <Image className='md:w-[360px] hidden md:block md:h-[380px]'  src={logo} alt="" />
       </div>
-      <div>
+      <div className=' md:py-20 md:px-20'> 
         <p>Our Team</p>
         <div>
           <div>
@@ -26,7 +26,7 @@ function page() {
             <p>CEO</p>
             <p> REDD+ Specialist, experienced in Carbon Market and remote sensing applications in forestry with a Ph.D from IIT KGP</p>
             <div>
-              
+
             </div>
           </div>
         </div>
--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -4,6 +4,7 @@ import logo from "../../assets/logo.png"
 import Image from 'next/image'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Dialog } from '@headlessui/react'
+import Link from 'next/link'
 
 
 const navigation = [
@@ -19,10 +20,10 @@ function Navbar() {
           <header className=" ">
         <nav className="flex items-center justify-between p-6 py-2 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <Image src={logo} alt="" />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -36,9 +37,9 @@ function Navbar() {
           </div>
           <div className="hidden lg:flex  lg:gap-x-16">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="lg:text-lg font-semibold leading-6 text-gray-900">
+              <Link key={item.name} href={item.href} className="lg:text-lg font-semibold leading-6 text-gray-900">
                 {item.name}
-              </a>
+              </Link>
             ))}
           </div>
           
@@ -47,10 +48,10 @@ function Navbar() {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <Link href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <Image src={logo} alt="" />
-              </a>
+              </Link>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -64,13 +65,13 @@ function Navbar() {
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
                
